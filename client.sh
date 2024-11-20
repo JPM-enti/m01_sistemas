91f8f3705a944dbff5ecedd77b9f19e719ad2052
JPM-enti <joel.parra@enti.cat>
2024-11-20 14:28:21 +0100

    hola

--- client.sh
+++ client.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+if [ "$1" == "" ]
+then
+	echo "Indica la direccion del servidor!"
+	echo "Ejemplo:"
+	echo -e "\t$0 10.40.1.174"
+	exit 1
+fi
 
+
+IP_SERVER=$1
+
+IP=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
 PORT="2022"
 
 
 echo "1. Envio de cabecera"
 
-echo "DMAM" | nc localhost $PORT
+echo "DMAM $IP" | nc $IP_SERVER $PORT
 
 DATA=`nc -l $PORT`
 
@@ -16,4 +27,15 @@ then
 	exit 1
 fi
 
-echo "placeholder" | nc localhost $PORT
+
+echo "3. Envio de nombre"
+echo "placeholder" | nc $IP_SERVER $PORT
+
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_NAME" ]
+then
+	echo "ERROR 2: Nombre enviado incorrecto"
+	exit 1
+fi
--- server.sh
+++ server.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-
 PORT="2022"
 
 
 echo "0. Empezar a escuchar"
 
 DATA=`nc -l $PORT`
+HEADER=`echo $DATA | cut -d " " -f 1`
+IP=`echo $DATA | cut -d " " -f 2`
 
-if [ "$DATA" != "DMAM" ]
+
+if [ "$HEADER" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost $PORT
+	echo "KO_HEADER" | nc $IP $PORT
 	exit 1
 fi
 
+echo "IP cliente: $IP"
 
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost $PORT
+echo "OK_HEADER" | nc $IP $PORT
 
 DATA=`nc -l $PORT`
+
+if [ "$DATA" == "" ]
+then
+	echo "ERROR 2: Nombre invalido"
+	echo "KO_NAME" | nc $IP $PORT
+	exit 1
+fi
+
+echo "4. CHECK OK - Enviando OK_NAME"
+echo "OK_NAME" | nc $IP $PORT
